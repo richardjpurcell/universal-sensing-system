8593feba881ed23f3187033b392dd9b0be617dcb
richardjpurcell <richard.purcell@dal.ca>
2024-12-17 10:12:46 -0400

    Multi-modal data analysis done

--- src/ERA5-download.py
+++ src/ERA5-download.py
@@ -1,24 +1,27 @@
 import cdsapi
 
-# Define dataset and request parameters
 dataset = "reanalysis-era5-land"
 request = {
-    "variable": ["2m_temperature"],
-    "year": ["2016"],
-    "month": ["04", "05"],
-    "day": ["30", "01", "02", "03"],
+    "variable": ["10m_v_component_of_wind"],
+    "year": "2016",
+    "month": "05",
+    "day": ["03"],
     "time": [
-        "00:00", "01:00", "02:00", "03:00", "04:00", "05:00",
-        "06:00", "07:00", "08:00", "09:00", "10:00", "11:00",
-        "12:00", "13:00", "14:00", "15:00", "16:00", "17:00",
-        "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
     ],
     "data_format": "netcdf",
-    "area": [57.2, -111.9, 56.2, -110.9]  # North, West, South, East
+    "download_format": "unarchived",
+    "area": [57.35, -112.05, 56.05, -110.75]
 }
 
-# Authenticate and retrieve the data
-client = cdsapi.Client()  # Ensure ~/.cdsapirc has the updated credentials
-client.retrieve(dataset, request, "era5_land_fort_mcmurray_2016.nc")
+client = cdsapi.Client()
 
-print("Data successfully downloaded: 'era5_land_fort_mcmurray_2016.nc'")
+target = "May-03-v-wind.nc"
+client.retrieve(dataset, request, target)
